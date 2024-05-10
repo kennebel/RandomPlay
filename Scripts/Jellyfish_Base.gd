c2e4b0b848a1b2a02488d1e658bdc76a61329beb
Jeremy A. Cunningham <jac@kennebel.com>
2024-05-09 21:27:12 -0600

    Learning scripting features

--- Scripts/Jellyfish_Base.gd
+++ Scripts/Jellyfish_Base.gd
@@ -3,11 +3,15 @@ extends Node3D
 @export var limit: float = 1
 var startY: float = 0
 var moveDir: int = 1
+var driftAway: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Jelly man!")
 	startY = position.y
+	driftAway.x = position.x
+	driftAway.y = position.z
+	driftAway = driftAway.normalized()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,4 +19,5 @@ func _process(delta):
 	if position.y > (startY+limit) || position.y < (startY-limit):
 		moveDir *= -1
 	position.y += delta * speed * moveDir
-	
+	position.x += driftAway.x * delta * (speed)
+	position.z += driftAway.y * delta * (speed)
