f6e1abba5b07a515407f373a6815d14439fed834
Jeremy A. Cunningham <jac@kennebel.com>
2024-06-02 09:01:00 -0600

    Improved swimming animation

--- Scripts/FishBase.gd
+++ Scripts/FishBase.gd
@@ -2,24 +2,33 @@ extends FlockBase
 
 class_name FishBase
 
-@export var SwimLimit:float = 1
+@export var SwimLimit:float = 0.25
 var MoveDir:int = 1
 var ModelView:Node3D = null
 
-# Called when the node enters the scene tree for the first time.
+var time_out:float = 1.0
+var time_since:float = 0.0
+var format_NameNum:String = "%s: %5.5f"
+
 func _ready():
-	ItemType = RandGlobal.ItemType.Fish
 	ModelView = get_node("Model")
+	ModelView.rotation.y = randf_range((SwimLimit * -1), SwimLimit)
 	super()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func DoMovement(delta:float):
 	super(delta)
 	
 	# Fish Swimming Motion
+	ModelView.rotate(Vector3.UP, delta*Speed*-0.25*MoveDir)
+	if ModelView.rotation.y > SwimLimit:
+		MoveDir = 1
+	elif ModelView.rotation.y < (SwimLimit * -1):
+		MoveDir = -1
 	
-	ModelView.rotate(Vector3.UP, delta*Speed*-0.25)
+	"""
+	time_since += delta
+	if time_since > time_out:
+		print(format_NameNum % [name, ModelView.rotation.y])
+		time_since = 0.0
+	"""
--- Scripts/RandGlobal.gd
+++ Scripts/RandGlobal.gd
@@ -13,27 +13,31 @@ var ItemCount: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+
 
 func GetNextID() -> int:
 	ItemCount += 1
 	return ItemCount
+
 
 func RegisterItem(newReg: FlockBase):
 	if !Items.has(newReg.ItemType):
 		Items[newReg.ItemType] = {}
 	if (!Items[newReg.ItemType].has(newReg.ID)):
 		Items[newReg.ItemType][newReg.ID] = newReg
-	
+
 
 func UnregisterItem(oldReg: FlockBase):
 	if Items.has(oldReg.ItemType) and Items[oldReg.ItemType].has(oldReg.ID):
 		Items[oldReg.ItemType].erase(oldReg.ID)
 
+
 func GroupCount(forType: ItemType) -> int:
 	if Items.has(forType):
 		return Items[forType].size
 	return 0
+
 
 func GroupItems(forType: ItemType):
 	if Items.has(forType):
